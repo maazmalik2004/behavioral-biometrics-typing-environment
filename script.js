29796ec1e9a7b23efe909477bf23380d52b53c94
Maaz Malik <maazmalik2004@gmail.com>
2025-01-07 19:00:22 +0530

    cleaned up code a little

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-let typing_area = document.getElementById("typing_area")
-let progress = document.getElementById("progress")
-let out_of_focus_warning = document.getElementById("out_of_focus_warning")
 let informatic = document.getElementById("informatic") 
-
+let progress = document.getElementById("progress")
+let typing_area = document.getElementById("typing_area")
+let out_of_focus_warning = document.getElementById("out_of_focus_warning")
 
 const paragraph = "  The sun dipped below the horizon, painting the sky in hues of orange and pink, as a gentle breeze rustled through the trees. Birds chirped their evening songs, signaling the close of another day. Children laughed and played in the distance, their joyful voices carrying through the air. A lone cyclist pedaled down the quiet street, the hum of tires on asphalt blending with the sounds of the settling world. The aroma of freshly baked bread wafted from a nearby home, inviting thoughts of warmth and comfort. In that moment, time seemed to slow, offering a brief respite from the rush of life."
 
@@ -10,27 +9,23 @@ let total_length = paragraph.replace(" ","").length
 let current_length = 0;
 progress.textContent = current_length+"/"+total_length
 
-document.getElementById("out_of_focus_warning").innerHTML = 
-    '<i class="fa-solid fa-triangle-exclamation"></i> click on paragraph to focus';
+informatic.textContent = "No rush ! This is not a speed test. Type naturally. This is for research purposes :) "
 
 typing_area.focus()
 
-informatic.textContent = "No rush ! This is not a speed test. Type naturally. This is for research purposes :) "
+document.getElementById("out_of_focus_warning").innerHTML = 
+    '<i class="fa-solid fa-triangle-exclamation"></i> click on paragraph to focus';
 
 function preprocess_paragraph_into_dom(paragraph, parent){
     let words = paragraph.trim().toLowerCase().split(" ")
 
     for(let word of words){
         let current_word_element = document.createElement("word")
-        current_word_element.style.display = "inline-block";
-        current_word_element.style.marginRight = "1rem";
-        current_word_element.style.marginBottom = "1rem";
         parent.appendChild(current_word_element)
+
         let letters = word.split("")
-        
         for(let letter of letters){
             let current_letter_element = document.createElement("letter")
-            current_letter_element.style.display = "inline-block";
             current_letter_element.textContent = letter
             current_word_element.appendChild(current_letter_element)
         }
@@ -41,17 +36,15 @@ preprocess_paragraph_into_dom(paragraph,typing_area)
 
 let current_word_element = typing_area.firstChild
 let current_letter_element = current_word_element.firstChild
-current_letter_element.style.textDecoration = "underline"
+current_letter_element.classList.add("cursor")
 
 typing_area.addEventListener("keydown", handle_keydown)
 
 function handle_keydown(event){
     event.key = event.key.toLowerCase()
-    // we will have to limit the number of valid keys
+
     if(event.key != " " && event.key != 'Backspace'){
-        // console.log(event.key)
-        if(current_letter_element != null){
-            console.log("current letter exists")
+        if(current_letter_element){
             if(event.key == current_letter_element.textContent){
                 current_length = current_length + 1
                 progress.textContent = current_length+"/"+total_length
@@ -59,34 +52,35 @@ function handle_keydown(event){
             }
             else
                 current_letter_element.classList.add("incorrect")
-            current_letter_element.style.textDecoration = "none"
+            current_letter_element.classList.remove("cursor")
             current_letter_element = current_letter_element.nextSibling
             if(current_letter_element)
-                current_letter_element.style.textDecoration = "underline"
+                current_letter_element.classList.add("cursor")
         }else{
             console.log("next sibling doesnt exist")
             extra_letter_element = document.createElement("letter")
             extra_letter_element.textContent = event.key
-            extra_letter_element.className = "extra"
+            extra_letter_element.classList.add("extra")
             current_word_element.appendChild(extra_letter_element)
             current_letter_element = extra_letter_element.nextSibling
             if(current_letter_element)   
-                current_letter_element.style.textDecoration = "underline"
+                current_letter_element.classList.add("cursor")
         }
     }
     else if(event.key == 'Backspace'){
+        // do nothing
     }else{
-        console.log("space/backspace pressed")
+        // key pressed is a space
         if(current_word_element.nextSibling){
             if(current_letter_element)
-                current_letter_element.style.textDecoration = "none"
+                current_letter_element.classList.remove("cursor")
             current_word_element = current_word_element.nextSibling
             current_word_element.scrollIntoView({
                 behavior: 'smooth',
                 block: 'center'
-              });
+            });
             current_letter_element = current_word_element.firstChild
-            current_letter_element.style.textDecoration = "underline"
+            current_letter_element.classList.add("cursor")
         }else{
             console.log("reached end of paragraph")
             typing_area.removeEventListener("keydown", handle_keydown)
