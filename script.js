581c9c7f64443a6ea40f3fd2e5a8f90c1b5002a1
Maaz Malik <maazmalik2004@gmail.com>
2025-01-09 00:11:33 +0530

    major modifications: handled for non alphabetical and special keys, created an enter name page

--- script.js
+++ script.js
@@ -2,19 +2,14 @@ let informatic = document.getElementById("informatic")
 let progress = document.getElementById("progress")
 let typing_area = document.getElementById("typing_area")
 let out_of_focus_warning = document.getElementById("out_of_focus_warning")
+let enter_name_container = document.getElementById("enter_name_container")
+let name_input = document.getElementById("name_input")
 
 const paragraph = "  The sun dipped below the horizon, painting the sky in hues of orange and pink, as a gentle breeze rustled through the trees. Birds chirped their evening songs, signaling the close of another day. Children laughed and played in the distance, their joyful voices carrying through the air. A lone cyclist pedaled down the quiet street, the hum of tires on asphalt blending with the sounds of the settling world. The aroma of freshly baked bread wafted from a nearby home, inviting thoughts of warmth and comfort. In that moment, time seemed to slow, offering a brief respite from the rush of life."
+// paragraph shall only be alphabetical and space in nature, no punctuations and no numbers
 
-let total_length = paragraph.replace(" ","").length
 let current_length = 0;
-progress.textContent = current_length+"/"+total_length
-
-informatic.textContent = "No rush ! This is not a speed test. Type naturally. This is for research purposes :) "
-
-typing_area.focus()
-
-document.getElementById("out_of_focus_warning").innerHTML = 
-    '<i class="fa-solid fa-triangle-exclamation"></i> click on paragraph to focus';
+let total_length = 0;
 
 function preprocess_paragraph_into_dom(paragraph, parent){
     let words = paragraph.trim().toLowerCase().split(" ")
@@ -25,67 +20,85 @@ function preprocess_paragraph_into_dom(paragraph, parent){
 
         let letters = word.split("")
         for(let letter of letters){
-            let current_letter_element = document.createElement("letter")
-            current_letter_element.textContent = letter
-            current_word_element.appendChild(current_letter_element)
+            const charCode = letter.charCodeAt(0);
+            if((charCode >= 65 && charCode <= 90) || (charCode >= 97 && charCode <= 122) || charCode === 32){
+                if((charCode >= 65 && charCode <= 90) || (charCode >= 97 && charCode <= 122)){
+                    total_length = total_length + 1;
+                }
+                let current_letter_element = document.createElement("letter")
+                current_letter_element.textContent = letter
+                current_word_element.appendChild(current_letter_element)
+            }
         }
     }
 }
 
 preprocess_paragraph_into_dom(paragraph,typing_area)
 
+progress.textContent = current_length+"/"+total_length
+informatic.textContent = "No rush! This is not a speed test. Type naturally with minimal distractions. This will be used for research purposes."
+document.getElementById("out_of_focus_warning").innerHTML = 
+    '<i class="fa-solid fa-triangle-exclamation"></i> click on paragraph to focus';
+
 let current_word_element = typing_area.firstChild
 let current_letter_element = current_word_element.firstChild
 current_letter_element.classList.add("cursor")
 
+typing_area.addEventListener("keydown", event=>{
+    // console.log(event.key, performance.now(), "clean")
+})
+
 typing_area.addEventListener("keydown", handle_keydown)
 
 function handle_keydown(event){
+    // console.log(event.key, performance.now(), "start")
     event.key = event.key.toLowerCase()
-
-    if(event.key != " " && event.key != 'Backspace'){
-        if(current_letter_element){
-            if(event.key == current_letter_element.textContent){
-                current_length = current_length + 1
-                progress.textContent = current_length+"/"+total_length
-                current_letter_element.classList.add("correct")
-            }
-            else
-                current_letter_element.classList.add("incorrect")
-            current_letter_element.classList.remove("cursor")
-            current_letter_element = current_letter_element.nextSibling
-            if(current_letter_element)
-                current_letter_element.classList.add("cursor")
-        }else{
-            console.log("next sibling doesnt exist")
-            extra_letter_element = document.createElement("letter")
-            extra_letter_element.textContent = event.key
-            extra_letter_element.classList.add("extra")
-            current_word_element.appendChild(extra_letter_element)
-            current_letter_element = extra_letter_element.nextSibling
-            if(current_letter_element)   
-                current_letter_element.classList.add("cursor")
-        }
-    }
-    else if(event.key == 'Backspace'){
-        // do nothing
-    }else{
-        // key pressed is a space
-        if(current_word_element.nextSibling){
-            if(current_letter_element)
+    charCode = event.key.charCodeAt(0)
+    if (event.key == " " || (event.key.length == 1 && charCode >= 97 && charCode <= 122)){
+        // console.log(event.key)
+        if(event.key != " "){
+            if(current_letter_element){
+                if(event.key == current_letter_element.textContent){
+                    current_length = current_length + 1
+                    progress.textContent = current_length+"/"+total_length
+                    current_letter_element.classList.add("correct")
+                }
+                else
+                    current_letter_element.classList.add("incorrect")
                 current_letter_element.classList.remove("cursor")
-            current_word_element = current_word_element.nextSibling
-            current_word_element.scrollIntoView({
-                behavior: 'smooth',
-                block: 'center'
-            });
-            current_letter_element = current_word_element.firstChild
-            current_letter_element.classList.add("cursor")
+                current_letter_element = current_letter_element.nextSibling
+                if(current_letter_element)
+                    current_letter_element.classList.add("cursor")
+            }else{
+                // console.log("next sibling doesnt exist")
+                extra_letter_element = document.createElement("letter")
+                extra_letter_element.textContent = event.key
+                extra_letter_element.classList.add("extra")
+                current_word_element.appendChild(extra_letter_element)
+                current_letter_element = extra_letter_element.nextSibling
+                if(current_letter_element)   
+                    current_letter_element.classList.add("cursor")
+            }
         }else{
-            console.log("reached end of paragraph")
-            typing_area.removeEventListener("keydown", handle_keydown)
+            // key pressed is a space
+            if(current_word_element.nextSibling){
+                if(current_letter_element)
+                    current_letter_element.classList.remove("cursor")
+                current_word_element = current_word_element.nextSibling
+                current_word_element.scrollIntoView({
+                    behavior: 'smooth',
+                    block: 'center'
+                });
+                current_letter_element = current_word_element.firstChild
+                current_letter_element.classList.add("cursor")
+            }else{
+                // console.log("reached end of paragraph")
+                typing_area.removeEventListener("keydown", handle_keydown)
+                handle_end_of_paragraph()
+            }
         }
     }
+    // console.log(event.key, performance.now(), "end")
 }
 typing_area.addEventListener("focus", function(){
     out_of_focus_warning.style.opacity = "0%"
@@ -94,6 +107,24 @@ typing_area.addEventListener("focus", function(){
 typing_area.addEventListener("blur", function(){
     out_of_focus_warning.style.opacity = "100%"
 })
+
+let username = ""
+
+function handle_name_submit(){
+    username = name_input.value
+    if(username.length > 0){
+        enter_name_container.style.opacity = "0%"
+        enter_name_container.style.pointerEvents = "none"
+        typing_area.focus()
+    }
+    else{
+        window.alert("Name cannot be empty")
+    }
+}
+
+function handle_end_of_paragraph(){
+    console.log("reached end of paragraph")
+}
 
 //we need to use pointer approach... 
 //if letter typed, move to the next letter if it exists
