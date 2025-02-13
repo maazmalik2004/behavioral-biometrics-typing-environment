8536ec905e361161847a9fa30f78e62e352178a5
shrihari-joshi <joshishrihari13@gmail.com>
2025-02-13 23:24:26 +0530

    mobile disabled -v2

--- script.js
+++ script.js
@@ -8,19 +8,6 @@ let think_typing_container = document.getElementById("think_typing_container")
 let question = document.getElementById("question")
 let thinking_typing_area = document.getElementById("thinking_typing_area")
 
-let name_input = document.getElementById("name_input")
-let email_input = document.getElementById("email_input")
-let gender_input = document.getElementById("gender_input")
-let employed_input = document.getElementById("employed_input")
-let age_input = document.getElementById("age_input")
-let programmer_input = document.getElementById("programmer_input")
-
-
-if (!name_input || !email_input || !gender_input || !employed_input || !age_input || !programmer_input) {
-    alert("Please fill in all fields before proceeding.");
-    return;
-}
-
 
 const paragraph = "The sun dipped below the horizon, painting the sky in hues of orange and pink, as a gentle breeze rustled through the trees. Birds chirped their evening songs, signaling the close of another day. Children laughed and played in the distance, their joyful voices carrying through the air. A lone cyclist pedaled down the quiet street, the hum of tires on asphalt blending with the sounds of the settling world. The aroma of freshly baked bread wafted from a nearby home, inviting thoughts of warmth and comfort. In that moment, time seemed to slow, offering a brief respite from the rush of life."
 
@@ -146,13 +133,16 @@ typing_area.addEventListener("blur", function () {
 let username = ""
 
 function handle_name_submit() {
-    username = name_input.value
-    if (username.length > 0) {
-        document.getElementById('enter_name_container').style.display = 'none';
-        document.getElementById('question_container').style.display = 'block';
-    }
-    else {
-        window.alert("Name cannot be empty")
+    let name_input = document.getElementById("name_input")
+    let email_input = document.getElementById("email_input")
+    let gender_input = document.getElementById("gender_input")
+    let employed_input = document.getElementById("employed_input")
+    let age_input = document.getElementById("age_input")
+    let programmer_input = document.getElementById("programmer_input")
+
+
+    if (!name_input || !email_input || !gender_input || !employed_input || !age_input || !programmer_input) {
+        alert("Please fill in all fields before proceeding.");
     }
 }
 
